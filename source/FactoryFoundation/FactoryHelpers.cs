44cda328922609ffb7a0438dee6e9fe99e6359f7
Michael Bradvica <mjbradvica@gmail.com>
2024-09-15 16:06:41 -0400

    added helpers

---
+++ source/FactoryFoundation/FactoryHelpers.cs
@@ -0,0 +1,36 @@
+﻿// <copyright file="FactoryHelpers.cs" company="Simplex Software LLC">
+// Copyright (c) Simplex Software LLC. All rights reserved.
+// </copyright>
+
+using System;
+
+namespace FactoryFoundation
+{
+    /// <summary>
+    /// A set of helpers for factories.
+    /// </summary>
+    public static class FactoryHelpers
+    {
+        /// <summary>
+        /// Attempts to create a <typeparamref name="TEntity"/> via a <see cref="Func{TResult}"/>.
+        /// </summary>
+        /// <typeparam name="TEntity">The type of the entity.</typeparam>
+        /// <param name="creationFunc">A <see cref="Func{TResult}"/> that will yield a <typeparamref name="TEntity"/>.</param>
+        /// <returns>A <see cref="ValidationEnvelope{TEntity}"/> result.</returns>
+        public static ValidationEnvelope<TEntity> TryCreateValidate<TEntity>(Func<TEntity> creationFunc)
+        {
+            TEntity entity;
+
+            try
+            {
+                entity = creationFunc.Invoke();
+            }
+            catch (Exception exception)
+            {
+                return ValidationEnvelope<TEntity>.Failure(exception);
+            }
+
+            return ValidationEnvelope<TEntity>.Success(entity);
+        }
+    }
+}
