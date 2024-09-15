05cfb2758b59c8d076e50a5460d17d8193341241
Michael B <mjbradvica@gmail.com>
2024-09-15 17:50:04 -0400

    added more tests

---
+++ tests/FactoryFoundation.Tests/FactoryHelperTests.cs
@@ -0,0 +1,40 @@
+﻿// <copyright file="FactoryHelperTests.cs" company="Simplex Software LLC">
+// Copyright (c) Simplex Software LLC. All rights reserved.
+// </copyright>
+
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FactoryFoundation.Tests
+{
+    /// <summary>
+    /// Tests for the <see cref="FactoryHelpers"/> class.
+    /// </summary>
+    [TestClass]
+    public class FactoryHelperTests
+    {
+        /// <summary>
+        /// Successful creation returns the newly created entity.
+        /// </summary>
+        [TestMethod]
+        public void TryCreateValidate_OnSuccess_ReturnsEntity()
+        {
+            const string result = "result";
+
+            var envelope = FactoryHelpers.TryCreateValidate(() => result);
+
+            Assert.AreEqual(result, envelope.Entity);
+        }
+
+        /// <summary>
+        /// Exceptions are correctly handled in method.
+        /// </summary>
+        [TestMethod]
+        public void TryCreateValidate_OnException_CatchesCorrectly()
+        {
+            var envelope = FactoryHelpers.TryCreateValidate<string>(() => throw new NullReferenceException());
+
+            Assert.IsInstanceOfType<NullReferenceException>(envelope.Exception);
+        }
+    }
+}
